a85229628973c5cf15048a781e92779d5e57fae3
mattrosendin <matthew.rosendin@gmail.com>
2020-10-23 22:52:02 -0700

    Modify fullscreen plugin

--- js/plugins/Fullscreen.js
+++ js/plugins/Fullscreen.js
@@ -6,19 +6,31 @@
  * @plugindesc Starts the game in fullscreen
  * @author Christian Schicho
  *
- * @help 
+ * @help
  */
  
 ;(function() {
-    function extend(obj, name, func) {
-        var orig = obj.prototype[name]
-        obj.prototype[name] = function() {
-            orig.call(this)
-            func.call(this)
-        }
+  function extend(obj, name, func) {
+    var orig = obj.prototype[name]
+    obj.prototype[name] = function() {
+      orig.call(this)
+      func.call(this)
     }
+  }
+ 
+  extend(Scene_Boot, 'start', function() {
+		Graphics._switchFullScreen();
+  })
+  
+  
+ var _Scene_Base_create = Scene_Base.prototype.create;
 
-    extend(Scene_Boot, 'start', function() {
-        Graphics._switchFullScreen();
-    })   
+	Scene_Base.prototype.create = function() {
+		_Scene_Base_create.call(this);
+		Graphics.width = 1152;
+		Graphics.height = 648;	
+		Graphics.boxHeight = 648;
+		Graphics.boxWidth = 1152;	
+	};
+ 
 })()
