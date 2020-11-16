055a3dc7c49a751e011844bef58ec997aa2f4612
mattrosendin <matthew.rosendin@gmail.com>
2020-11-16 11:15:01 -0800

    0.5.30

--- js/plugins/BBS_MultiTouch.js
+++ js/plugins/BBS_MultiTouch.js
@@ -16,18 +16,18 @@
  * @plugindesc v1.02 Overrides TouchInput core scripts to provide true multi-touch
  * and gesture support for RPGMaker MV.  Originally developed in conjunction with
  * BBS_MapControls.
- * 
+ *
  * Special Thanks to Tsukihime for all the help.
  * Special Thanks to 'Ramza' Michael Sweeney and Lakaroth for helping so much with testing.
  * Special Thanks to Jairaks89 for https://github.com/jairajs89/Touchy.js/blob/master/touchy.js
  *  on which the multi-touch structure was loosely based.
- * 
+ *
  * ============================================================================
  * Terms of Use
  * ============================================================================
  *  - Free for use in non-commercial projects with credits
  *  - Contact me for commercial use
- * 
+ *
  * ============================================================================
  * Parameters
  * ============================================================================
@@ -35,25 +35,25 @@
  * @desc Enable to activate console variable logging.  Use for debugging odd behaviour.
  * true to enable console variable logging.
  * @default false
- * 
+ *
  * @param -----Input History Tracking-----
  * @param Button History Max. Size
  * @desc How many buttons to track at once.  Keep small.  Use value of 1 for single touch, 2+ for multi-touch.
  * @default 2
- * 
+ *
  * @param -----Gesture Handling-----
  * @param Gesture Distance Threshold
  * @desc Minimum distance touch travels (in pixels) to be considered a swipe gesture.
  * @default 150
- * 
+ *
  * @param Gesture Distance Restraint
  * @desc Maximum distance touch can travel (in pixels) in perpendicular direction to swipe to be considered a swipe.
  * @default 100
- * 
+ *
  * @param Gesture Maximum Time
  * @desc Maximum time before a touch times out, and ceases to be considered a gesture (in milliseconds).
  * @default 300
-  * 
+  *
  * @help
  * ============================================================================
  * Description
@@ -67,9 +67,9 @@
  * Script Commands
  * ============================================================================
  * Script Commands:
- * 
+ *
  *	 TouchInput.wasGestured('swipe left');			# Returns true if swipe left was gestured most recently.
- * 
+ *
  * ============================================================================
  * Change Log
  * ============================================================================
@@ -86,35 +86,37 @@ Imported.MultiTouch = 1;
 BBS.MultiTouch = BBS.MultiTouch || {};
 
 (function() {
-	
-   	//=============================================================================
+  //=============================================================================
 	// Parameter Variables
 	//=============================================================================
-	var parameters = PluginManager.parameters('BBS_MultiTouch');
-	var pDebugging		  = eval(String(parameters['Debug Mode'] || 'false'));	
-	var pBtnHistoryMaxSize= Number(parameters['Button History Max. Size'] || '2');
-	var pThreshold		  = Number(parameters['Gesture Distance Threshold'] || '150');
-	var pRestraint		  = Number(parameters['Gesture Distance Restraint'] || '100');
-	var pAllowedTime	  = Number(parameters['Gesture Maximum Time'] || '300');
+	var parameters         = PluginManager.parameters('BBS_MultiTouch');
+	var pDebugging		     = eval(String(parameters['Debug Mode'] || 'false'));
+	var pBtnHistoryMaxSize = Number(parameters['Button History Max. Size'] || '2');
+	var pThreshold		     = Number(parameters['Gesture Distance Threshold'] || '150');
+	var pRestraint		     = Number(parameters['Gesture Distance Restraint'] || '100');
+	var pAllowedTime	     = Number(parameters['Gesture Maximum Time'] || '300');
 
 	//=============================================================================
 	// Input/Button History
 	//=============================================================================
 	function InputHistory() {
+		console.log('Initializing InputHistory')
 		this.initialize.apply(this, arguments);
 	};
-	
+
 	InputHistory.prototype = Object.create(InputHistory.prototype);
 	InputHistory.prototype.constructor = InputHistory;
-	
+
 	InputHistory.prototype.initialize = function() {
 		this._btnHistory = Array();
 		this._swipeHistory = null;
+		console.log('Intialized empty button and swipe histories')
 	};
-	
-	InputHistory.prototype.wasPressed = function(btn) {		
+
+	InputHistory.prototype.wasPressed = function(btn) {
+		console.log('Checking wasPressed', btn)
 		if (this._btnHistory.length < 1) return false;
-		
+		console.log('btnHistory', this._btnHistory)
 		var pressed = false;
 		for (var i = 0; i < this._btnHistory.length; i++) {
 			if (this._btnHistory[i] === btn) {
@@ -122,66 +124,67 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 				break;
 			}
 		}
-		
+
 		return pressed;
 	};
-	
+
 	InputHistory.prototype.wasGestured = function(gesture) {
 		return (this._swipeHistory === gesture);
 	};
-	
-	InputHistory.prototype.addBtn = function(btn) {		
+
+	InputHistory.prototype.addBtn = function(btn) {
+		console.log('Adding button', btn)
 		// If btn already stored, don't need to store it again.
 		if (!this.wasPressed(btn)) {
 			// Shift if history size exceeded.
 			if (this._btnHistory.length >= pBtnHistoryMaxSize) {
 				this._btnHistory.shift();
 			}
-			
+
 			this._btnHistory.push(btn);
-			
-			if (pDebugging) {				
+
+			if (pDebugging) {
 				$gameScreen.showPicture(1, this._btnHistory[0], 0, 0, 0, 100, 100, 255, 0);
-				
+
 				if (this._btnHistory.length > 1) {
 				    $gameScreen.showPicture(2, this._btnHistory[1], 0, 40, 0, 100, 100, 255, 0);
 				}
 			}
-			
+
 		}
 	};
-	
+
 	InputHistory.prototype.addGesture = function(gesture) {
 		this._swipeHistory = gesture;
 	};
-	
+
 	InputHistory.prototype.removeBtn = function(btn) {
 		var index = this._btnHistory.indexOf(btn);
 		if (index >= 0) {
 			this._btnHistory.splice(index, 1);
 		}
 	};
-	
+
 	InputHistory.prototype.removeGesture = function(gesture) {
 		this._swipeHistory = null;
 	};
-	
+
 	InputHistory.prototype._clear = function() {
-		if (pDebugging) {				
+		if (pDebugging) {
 			$gameScreen.erasePicture(1);
-				
+
 			if (this._btnHistory.length > 1) {
 			   $gameScreen.erasePicture(2);
 			}
 		}
-		
+
 		this._btnHistory = [];
 		this._swipeHistory = null;
 	};
 
 	//=============================================================================
 	// Finger - Object to manage single-finger touch interactions
-	//=============================================================================	
+	//=============================================================================
 	function Finger(id) {
 		this.id = id;
 		this._x = null;
@@ -189,14 +192,14 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 		this._startX = null;
 		this._startY = null;
 		this._date = 0;
-		
+
 		// Release not stored, because released fingers are deleted.
 	};
-	
+
 	//=============================================================================
 	// Touch Input New Functions: https://github.com/jairajs89/Touchy.js/blob/master/touchy.js
-	//=============================================================================		
-	
+	//=============================================================================
+
 	/* Create a new hand based on the current touches on the screen */
 	TouchInput.fingersRestart = function(touches) {
 		if (touches.length <= 0) {
@@ -204,29 +207,29 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 		}
 
 		this.fingers = [];
-		for (var i = 0; i < event.touches.length; i++) 
+		for (var i = 0; i < event.touches.length; i++)
 		{
 			var touch = event.touches[i];
 			var x = Graphics.pageToCanvasX(touch.pageX);
 			var y = Graphics.pageToCanvasY(touch.pageY);
-						
+
 			if (Graphics.isInsideCanvas(x, y)) {
-				var finger = new Finger(touch.identifier);	
+				var finger = new Finger(touch.identifier);
 				this.fingers.push(finger);
 				this._screenPressed = true;
-				
+
 				finger._x = x;
 				finger._startX = x;
 				finger._y = y;
 				finger._startY = y;
 				finger._date = Date.now();
 				finger._pressedTime = 0;
-				
+
 				if (pDebugging) {
 					console.log("Touch trigger activated");
 					console.log(this.fingers);
 				}
-				
+
 				this._onTrigger(x, y);
 			}
 		}
@@ -242,27 +245,34 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 			finger._pressedTime++;
 		}
 	};
-	
+
+	TouchInput.checkForDash = function() {
+		$gameSystem.disableMenu()
+		if (this._history.wasPressed('cancel')) {
+			console.log('test')
+		}
+	}
+
 	/* Check a finger just prior to release to detect whether it was a swipe. */
 	TouchInput.checkForSwipe = function(finger) {
 		if (!finger) {
 			return;
 		}
-		
+
 		var elapsedTime = new Date().getTime() - finger._date;	// get time elapsed
 		if (elapsedTime > pAllowedTime) {
 			return;
 		}
-		
+
 		var distX = finger._x - finger._startX; 				// get horizontal dist traveled by finger while in contact with surface
 		var distY = finger._y - finger._startY; 				// get vertical dist traveled by finger while in contact with surface
-		
+
 		if (pDebugging) {
 			console.log(elapsedTime + " vs. " + pAllowedTime);
 			console.log("X Dist: " + distX + " Y Dist: " + distY);
 			console.log("Threshold: " + pThreshold + " Restraint: " + pRestraint);
 		}
-		
+
 		if (Math.abs(distX) >= pThreshold && Math.abs(distY) <= pRestraint) { // 2nd condition for horizontal swipe met
 			// If dist traveled is negative, it indicates left swipe
 			if (distX < 0) {
@@ -279,10 +289,10 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 				this._onSwipedDown();
 			}
 		}
-		
+
 		return;
 	};
-	
+
 	/* Destroy the current hand regardless of fingers on the screen */
 	TouchInput.fingersDestroy = function() {
 		if (!this.fingers) {
@@ -291,7 +301,7 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 
 		for (var i = 0; i < this.fingers.length; i++) {
 			var finger = this.fingers[i];
-			
+
 			// Gesture handling happens here.
 			this.checkForSwipe(finger);
 			this._onRelease(finger._x, finger._y);
@@ -300,13 +310,13 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 		this._screenPressed = false;
 		this.fingers = null;
 	};
-	
+
 	/* Get finger by id */
 	TouchInput.getFinger = function(id) {
 		var foundFinger = null;
 
 		if (!this.fingers) return foundFinger;
-		
+
 		// Direct iteration significantly faster than foreach.
 		for(var i = 0; i < this.fingers.length; i++) {
 			if (this.fingers[i].id === id) {
@@ -316,67 +326,69 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 		}
 
 		return foundFinger;
-	};	
-	
+	};
+
 	/* last touch.x and .y will not work with multi-touch.  Need to override. */
 	TouchInput.isTouchingArea = function(rect) {
+		console.log('Checking isTouchingArea', rect)
 		if (this.fingers === null) return false;
-		
+
 		for (var i = 0; i < this.fingers.length; i++) {
 			var finger = this.fingers[i];
 			if (pDebugging) {
 				var strOut = "Finger at: " + finger._x + "; " + finger._y + " vs. touch area: " + rect.x + "; " + rect.y;
 				console.log(strOut);
 			}
-			
+
 			if (rect.contains(finger._x, finger._y)) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	};
-	
+
 	/* last touch.x and .y used only for mouse click.  Required function, mouse input was non-functional.*/
 	TouchInput.isClicked = function(rect) {
+		console.log('Checking isClicked', rect)
 		if ( !this.isPressed() ) return false;
-		
+
 		if (pDebugging) {
 			var strOut = "Click at: " + this._x + "; " + this._y + " vs. click area: " + rect.x + "; " + rect.y;
 			console.log(strOut);
 		}
-		
+
 		if (rect.contains(this._x, this._y)) {
 			return true;
 		}
-		
+
 		return false;
 	};
-	
+
 	/* Input check for directional swipe */
 	TouchInput.swipedLeft = function() {
 		var swipedLeft = this._history.wasGestured('swipe left');
 		return swipedLeft;
 	};
-	
+
 	/* Input check for directional swipe */
 	TouchInput.swipedRight = function() {
 		var swipedRight = this._history.wasGestured('swipe right');
 		return swipedRight;
 	};
-	
+
 	/* Input check for directional swipe */
 	TouchInput.swipedUp = function() {
 		var swipedUp = this._history.wasGestured('swipe up');
 		return swipedUp;
 	};
-	
+
 	/* Input check for directional swipe */
 	TouchInput.swipedDown = function() {
 		var swipedDown = this._history.wasGestured('swipe down');
 		return swipedDown;
 	};
-	
+
 	/**
 	 * @static
 	 * @method _onSwipeLeft
@@ -389,7 +401,7 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 			console.log("Left swipe detected!");
 		}
 	};
-	
+
 	/**
 	 * @static
 	 * @method _onSwipeRight
@@ -402,7 +414,7 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 			console.log("Right swipe detected!");
 		}
 	};
-	
+
 	/**
 	 * @static
 	 * @method _onSwipeUp
@@ -415,7 +427,7 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 			console.log("Up swipe detected!");
 		}
 	};
-	
+
 	/**
 	 * @static
 	 * @method _onSwipeDown
@@ -428,7 +440,7 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 			console.log("Down swipe detected!");
 		}
 	};
-	
+
 	TouchInput.prototype.isGestured = function(gest) {
 		var gestSafe = gest.toLowerCase();
 		var pressed = this._history.wasGestured(gestSafe);
@@ -436,23 +448,24 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 			if (pDebugging) console.log(gestSafe);
 			this._history.removeGesture(gestSafe);
 		}
-		
+
 		return pressed;
 	};
-	
+
 	/* Adds button to input history if not already in input history. */
 	TouchInput.addLastButton = function(btn) {
+		console.log('Running addLastButton', btn)
 		this._history.addBtn(btn);
 	};
-	
+
 	/* Clears input history. */
 	TouchInput._clearHistory = function() {
 		this._history._clear();
 	};
-	
+
 	//=============================================================================
 	// Touch Input Core Overrides
-	//=============================================================================	
+	//=============================================================================
 
 	/**
 	 * Initializes the touch system.
@@ -461,12 +474,13 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 	 * @method initialize
 	 */
 	var BBS_MC_TouchInputInitialize = TouchInput.initialize;
-    TouchInput.initialize = function() {
+  TouchInput.initialize = function() {
+		console.log('Running TouchInput.initialize')
 		BBS_MC_TouchInputInitialize.call(this, arguments);
 		this._history = new InputHistory();
 		this.fingers = null;
 	};
-	
+
 	/**
 	 * @static
 	 * @method _onTouchStart
@@ -475,11 +489,13 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 	 */
 	// Replaces core functionality.  Do not modify!
 	TouchInput._onTouchStart = function(event) {
+		console.log('Running TouchInput._onTouchStart')
+		// if ($dataMap) {
+		// 	this.checkForDash()
+		// }
 		this.fingersDestroy();
 		this.fingersRestart(event.touches);
-		if (window.cordova || window.navigator.standalone) {
-			event.preventDefault();
-		}
+		event.preventDefault();
 	};
 
 	/**
@@ -490,6 +506,7 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 	 */
 	// Replaces core functionality.  Do not modify!
 	TouchInput._onTouchMove = function(event) {
+		console.log('Running TouchInput._onTouchMove')
 		// Each changed touch for this event represents a moved touch.
 		// Direct iteration significantly faster than foreach.
 		for (var i = 0; i < event.changedTouches.length; i++) {
@@ -498,16 +515,16 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 			if( !finger ) {
 				return;
 			}
-			
+
 			var x = Graphics.pageToCanvasX(touch.pageX);
 			var y = Graphics.pageToCanvasY(touch.pageY);
 			finger._x = x;
 			finger._y = y;
-			
+
 			this._onMove(x, y);
 		}
 	};
-	
+
 	/**
 	 * @static
 	 * @method _onTouchEnd
@@ -516,13 +533,14 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 	 */
 	// Replaces core functionality.  Do not modify!
 	TouchInput._onTouchEnd = function(event) {
+		console.log('Running TouchInput._onTouchEnd')
 		this.fingersDestroy();
 
 		var remainingTouches = [];
 		// Direct iteration significantly faster than foreach.
 		for (var i = 0; i < event.touches.length; i++) {
 			var unChanged = true;
-			
+
 			// Each changed touch for this event represents a released touch.
 			for (var j = 0; j < event.changedTouches.length; j++) {
 				if (event.changedTouches[j].identifier === event.touches[i].identifier) {
@@ -530,12 +548,12 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 					break;
 				}
 			}
-			
+
 			if (unChanged) {
 				remainingTouches.push(event.touches[i]);
 			}
 		}
-		
+
 		this.fingersRestart(remainingTouches);
 		event.preventDefault();
 	};
@@ -559,7 +577,7 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 		this._swipedDown = false;
 		this.fingersDestroy();
 	};
-	
+
 	/**
 	 * Updates the touch data.
 	 *
@@ -578,8 +596,8 @@ BBS.MultiTouch = BBS.MultiTouch || {};
 		this._events.swipedUp = false;
 		this._events.swipedDown = false;
 	};
-	
+
 })(BBS.MultiTouch);
 //=============================================================================
 // End of File
-//=============================================================================	
+//=============================================================================
