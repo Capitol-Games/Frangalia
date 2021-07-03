c4418eb9d8f4863b1be2c68d2d192af4dcdb8c73
mattrosendin <matthew.rosendin@gmail.com>
2021-07-03 01:09:37 -0700

    add message eval text plugin

--- js/plugins/Fullscreen.js
+++ js/plugins/Fullscreen.js
@@ -1,14 +1,14 @@
 //=============================================================================
 // Fullscreen.js
 //=============================================================================
- 
+
 /*:
  * @plugindesc Starts the game in fullscreen
  * @author Christian Schicho
  *
  * @help
  */
- 
+
 ;(function() {
   function extend(obj, name, func) {
     var orig = obj.prototype[name]
@@ -17,20 +17,23 @@
       func.call(this)
     }
   }
- 
+
   extend(Scene_Boot, 'start', function() {
-		Graphics._switchFullScreen();
+    // Only run fullscreen on mobile device
+    if (!Utils.isOptionValid('test')) {
+      Graphics._switchFullScreen();
+    }
   })
-  
-  
+
+
  var _Scene_Base_create = Scene_Base.prototype.create;
 
 	Scene_Base.prototype.create = function() {
 		_Scene_Base_create.call(this);
 		Graphics.width = 1152;
-		Graphics.height = 648;	
+		Graphics.height = 648;
 		Graphics.boxHeight = 648;
-		Graphics.boxWidth = 1152;	
+		Graphics.boxWidth = 1152;
 	};
- 
+
 })()
